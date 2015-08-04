0b81db08d5d362f8550d9489b514b0d3a1b9472c
nitya <n.narasimhan@gmail.com>
2015-08-04 14:36:38 -0400

    Basic cards added ...

---
+++ meteor-app/client/views/pages/home.js
@@ -0,0 +1,23 @@
+Template.home.helpers({
+	cards: function(){
+		return [1,2,3,4,5,6,7,8,9,10];
+	}
+});
+
+/*
+
+	<div class="demo-grid-1 mdl-grid">
+	  <div class="mdl-cell mdl-cell--3-col">
+	  	{{>cardSquare }}
+	  </div>
+	  <div class="mdl-cell mdl-cell--3-col">
+	  	{{>cardEvent }}
+	  </div>
+	  <div class="mdl-cell mdl-cell--3-col">
+	  	{{>cardImage }}
+	  </div>
+	  <div class="mdl-cell mdl-cell--3-col">
+	  	{{>cardEvent }}
+	  </div>
+	</div>
+*/
